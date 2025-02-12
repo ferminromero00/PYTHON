ea111576aae99880ada083c4790e3795beff090c
Fermin <ferminromero2005@gmail.com>
2025-02-12 13:01:12 +0100

    corrige la sintaxis de importación y mejora la estructura del retorno en la vista index

--- agendaConSesiones/agenda_app/views.py
+++ agendaConSesiones/agenda_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from agenda_app.forms import Formulario;
+from agenda_app.forms import Formulario
 from agenda_app.models import Cita
 
 # Create your views here.
@@ -14,7 +14,7 @@ def index(request):
             fecha = form.cleaned_data["fecha"]
             cita = Cita(descripcion, fecha)
             request.session["agenda"].append(cita)
-        else:
-            form = Formulario()
-        
-        return render(request, 'index.html', {'form':form, 'agenda': request.session["agenda"]})
+    else:
+        form = Formulario()
+    
+    return render(request, 'index.html', {'form': form, 'agenda': request.session["agenda"]})
