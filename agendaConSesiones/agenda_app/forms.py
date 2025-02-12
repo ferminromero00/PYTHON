80b1257ea7d2d39bc73ada724b6312ef5861a76f
Fermin <ferminromero2005@gmail.com>
2025-02-12 13:15:11 +0100

    modifica el campo 'fecha' en el formulario para usar un widget de entrada de tipo fecha

--- agendaConSesiones/agenda_app/forms.py
+++ agendaConSesiones/agenda_app/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class Formulario(forms.Form):
     descripcion = forms.CharField(required=False)
-    fecha = forms.DateField(required=False, input_formats=['%d-%m-%Y'])
+    fecha = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}), input_formats=['%d-%m-%Y'])
