efe193b40b9a46ae0f8c722fd47a58c9bd5c610d
Fermin <ferminromero2005@gmail.com>
2025-02-12 13:26:20 +0100

    modifica la vista index para guardar citas como diccionarios y formatear la fecha en 'dd-mm-aaaa'

--- agendaConSesiones/agenda_app/views.py
+++ agendaConSesiones/agenda_app/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render
 from agenda_app.forms import Formulario
 from agenda_app.models import Cita
 
-# Create your views here.
 def index(request):
     if "agenda" not in request.session:
         request.session["agenda"] = []
-    
+
     if request.method == "POST":
         form = Formulario(request.POST)
         if form.is_valid():
             descripcion = form.cleaned_data["descripcion"]
             fecha = form.cleaned_data["fecha"]
-            cita = Cita(descripcion, fecha)
+
+            # Guardar la cita en formato de diccionario (serializable)
+            cita = {
+                "descripcion": descripcion,
+                "fecha": fecha.strftime("%d-%m-%Y") if fecha else None  # Convertir fecha a string
+            }
+
             request.session["agenda"].append(cita)
+            request.session.modified = True  # Indicar que la sesión ha cambiado
+
     else:
         form = Formulario()
-    
+
     return render(request, 'index.html', {'form': form, 'agenda': request.session["agenda"]})
