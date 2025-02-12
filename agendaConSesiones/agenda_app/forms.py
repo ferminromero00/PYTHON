c6f1e09389a450aff27336c92dbceb90152a0511
Fermin <ferminromero2005@gmail.com>
2025-02-12 13:04:39 +0100

    modifica el campo 'fecha' en el formulario para aceptar un formato de entrada específico y actualiza los comandos en el archivo de texto

--- agendaConSesiones/agenda_app/forms.py
+++ agendaConSesiones/agenda_app/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class Formulario(forms.Form):
     descripcion = forms.CharField(required=False)
-    fecha = forms.DateField(required=False)
+    fecha = forms.DateField(required=False, input_formats=['%d-%m-%Y'])
