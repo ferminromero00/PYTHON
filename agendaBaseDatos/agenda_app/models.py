5abc00c4f430ff8f5e0fea14559619bced3f8075
Fermin <ferminromero2005@gmail.com>
2025-02-12 14:28:07 +0100

    modifica el modelo Cita para cambiar el campo 'fecha' a DateField con longitud máxima

--- agendaBaseDatos/agenda_app/models.py
+++ agendaBaseDatos/agenda_app/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Cita(models.Model):
-    fecha = models.DateField(auto_now_add=True)
+    #fecha = models.DateField(auto_now_add=True)
+    fecha = models.DateField(max_length=100)
     descripcion = models.CharField()
