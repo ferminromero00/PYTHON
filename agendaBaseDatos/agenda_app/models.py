8e906785a7fd9cb211a1e59c5e311497a2abf477
Fermin <ferminromero2005@gmail.com>
2025-02-14 10:59:05 +0100

    mejora el formulario CitaForm y actualiza el modelo Cita para incluir un límite de longitud en la descripción

--- agendaBaseDatos/agenda_app/models.py
+++ agendaBaseDatos/agenda_app/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class Cita(models.Model):
     fecha = models.DateField(auto_now_add=True)
-    descripcion = models.CharField()
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.descripcion
