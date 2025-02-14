a08e88e59a37d7487520f81fddd0648babab78f8
Fermin <ferminromero2005@gmail.com>
2025-02-14 10:59:50 +0100

    elimina el campo 'fecha' del formulario CitaForm

--- agendaBaseDatos/agenda_app/forms.py
+++ agendaBaseDatos/agenda_app/forms.py
@@ -4,7 +4,4 @@ from .models import Cita
 class CitaForm(forms.ModelForm):
     class Meta:
         model = Cita
-        fields = ['descripcion', 'fecha']
-        widgets = {
-            'fecha': forms.DateInput(attrs={'type': 'date'})
-        }
+        fields = ['descripcion']
