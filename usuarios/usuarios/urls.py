e7a397b5f3d7111bd7ab73eb5029cd5b78d3afc3
Fermin <ferminromero2005@gmail.com>
2025-02-14 12:33:51 +0100

    agrega vista y plantilla para el registro de usuarios, y configura la navegación

--- usuarios/usuarios/urls.py
+++ usuarios/usuarios/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from usuarios_app.views import registro
 
 urlpatterns = [
+    path('registro/', registro, name='registro'),
     path('admin/', admin.site.urls),
 ]
--- usuarios/usuarios_app/forms.py
+++ usuarios/usuarios_app/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import get_user_model
-from usuarios_app import Usuario
+from usuarios_app.models import Usuario
 
 class RegistroForm(UserCreationForm):
     class Meta():
