bfb34c6ecc07e298722a967427fcd930df1d9ade
Fermin <ferminromero2005@gmail.com>
2025-02-14 11:03:33 +0100

    actualiza la importación de vistas en el archivo de URLs para utilizar el módulo correcto

--- agendaBaseDatos/agendaBaseDatos/urls.py
+++ agendaBaseDatos/agendaBaseDatos/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import index, cita, citas, editar_cita
+from agenda_app.views import index, cita, citas, editar_cita
 
 urlpatterns = [
     path('', index, name='index'),
