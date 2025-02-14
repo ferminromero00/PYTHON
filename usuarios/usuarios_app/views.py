a57e2465cf0fef09057e21cd4f4613b25797e438
Fermin <ferminromero2005@gmail.com>
2025-02-14 12:42:16 +0100

    corrige la validación del formulario en la vista de registro de usuarios

--- usuarios/usuarios_app/views.py
+++ usuarios/usuarios_app/views.py
@@ -3,11 +3,12 @@ from usuarios_app.forms import RegistroForm
 
 # Create your views here.
 def registro(request):
- if request.method == 'POST':
-   form = RegistroForm(request.POST)
-   if form.isValid():
-       usuario = form.save()
-       #login(request, usuario)
-   else:
-       form = RegistroForm()
-   return render(request, 'registro.html', {'form': form})    
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            # login(request, usuario)
+            return render(request, 'registro.html', {'form': form})
+    else:
+        form = RegistroForm()
+    return render(request, 'registro.html', {'form': form})
