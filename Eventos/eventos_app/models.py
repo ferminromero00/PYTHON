6848374c335010234bbcd3f25b161a1028de0aa7
Fermin <ferminromero2005@gmail.com>
2025-02-17 14:23:09 +0100

    agrega modelos y formularios para la gestión de eventos y usuarios

--- Eventos/eventos_app/models.py
+++ Eventos/eventos_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from usuarios_app.models import Usuario
 # Create your models here.
+class Evento(models.Model):
+	titulo = models.CharField(max_length=100)
+	descripcion = models.CharField(max_length=100)
+	fecha = models.DateField()
+	usuario = models.ForeignKey(Usuario)
