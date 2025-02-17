25b6de7a0c139dac34add9d12729f8886fc93c1e
Fermin <ferminromero2005@gmail.com>
2025-02-17 13:32:34 +0100

    agrega la ruta de cierre de sesión y actualiza el mensaje de bienvenida para mostrar el nombre de usuario

--- usuarios/usuarios/urls.py
+++ usuarios/usuarios/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('registro/', registro, name='registro'),
     path('inicio/', inicio, name='inicio'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
