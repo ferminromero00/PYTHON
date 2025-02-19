57e132ce67122b1151fe2b57334f4dfdbb356a8a
Fermin <ferminromero2005@gmail.com>
2025-02-19 12:13:39 +0100

    das

--- Eventos/Eventos/urls.py
+++ Eventos/Eventos/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('admin/', admin.site.urls),
 ]
