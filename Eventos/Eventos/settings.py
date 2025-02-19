b6b7fcd69febb366fc17e4b33f780ea2facd2902
Fermin <ferminromero2005@gmail.com>
2025-02-19 12:47:40 +0100

    configura el modelo de usuario personalizado y establece la URL de redirección tras el inicio de sesión

--- Eventos/Eventos/settings.py
+++ Eventos/Eventos/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "usuarios_app.Usuario"
+LOGIN_REDIRECT_URL = "/index/"
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
