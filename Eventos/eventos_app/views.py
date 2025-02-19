a7970456f16ace34ee952e3a6df55b21526d3d77
Fermin <ferminromero2005@gmail.com>
2025-02-19 13:40:07 +0100

    agrega funcionalidad para crear eventos y nueva plantilla para el formulario

--- Eventos/eventos_app/views.py
+++ Eventos/eventos_app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from eventos_app.models import Evento
+from eventos_app.forms import EventoForm
 
 # Create your views here.
 def index (request):
     eventos = Evento.objects.filter(usuario = request.user)
     return render(request, 'index.html', {'eventos': eventos})
+
+def crear_evento (request):
+    if(request.method == "POST"):
+        form = EventoForm(request.POST)
+        form.save()
+        return redirect('index')
+    else:
+        form = EventoForm()
+        
+    return render(request, 'nuevoevento.html', {'form': form})
