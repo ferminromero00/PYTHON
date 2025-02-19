5be4dea470f772bbad4d62063cd1e77508a92fc8
Fermin <ferminromero2005@gmail.com>
2025-02-19 12:11:13 +0100

    cambia la importación de auth_views a views en urls.py

--- Eventos/Eventos/urls.py
+++ Eventos/Eventos/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import auth_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
