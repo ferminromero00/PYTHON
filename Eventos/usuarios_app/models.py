caaadb4109d713c2d0cd095151b766849a206bd7
Fermin <ferminromero2005@gmail.com>
2025-02-19 12:14:23 +0100

    agrega campos groups y user_permissions al modelo Usuario

--- Eventos/usuarios_app/models.py
+++ Eventos/usuarios_app/models.py
@@ -2,5 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Usuario (AbstractUser):
+class Usuario(AbstractUser):
     telefono = models.CharField(max_length=20)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='usuarios_app_user_set',
+        related_query_name='usuario',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='usuarios_app_user_set',
+        related_query_name='usuario',
+        blank=True
+    )
