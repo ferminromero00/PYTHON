4d904d164bf13dafc34691e47e56ced8d6f021da
Fermin <ferminromero2005@gmail.com>
2025-02-19 12:06:35 +0100

    elimina on_delete=models.CASCADE del campo usuario en el modelo Evento

--- Eventos/eventos_app/models.py
+++ Eventos/eventos_app/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from usuarios_app.models import Usuario
-
 # Create your models here.
 class Evento(models.Model):
-    titulo = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=100)
-    fecha = models.DateField()
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)  
+	titulo = models.CharField(max_length=100)
+	descripcion = models.CharField(max_length=100)
+	fecha = models.DateField()
+	usuario = models.ForeignKey(Usuario)
