8c8b7578ec985f508c048ec96fc0694e65e4da81
Fermin <ferminromero2005@gmail.com>
2025-02-19 13:33:05 +0100

    filtra eventos por usuario en la vista de índice

--- Eventos/eventos_app/views.py
+++ Eventos/eventos_app/views.py
@@ -3,5 +3,5 @@ from eventos_app.models import Evento
 
 # Create your views here.
 def index (request):
-    eventos = Evento.objects.all
+    eventos = Evento.objects.filter(usuario = request.user)
     return render(request, 'index.html', {'eventos': eventos})
