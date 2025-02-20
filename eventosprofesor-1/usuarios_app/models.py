544ae4e8da7d8f242b34d2f3f63a910971dfd60d
Fermin <ferminromero2005@gmail.com>
2025-02-20 09:53:57 +0100

    dsadas

--- eventosprofesor-1/usuarios_app/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class Usuario(AbstractUser):
-    # Aquí puedes agregar campos adicionales si es necesario
-    pass
