3e467aae73d1d79242b0b64c48fc84de5ec1409b
Fermin <ferminromero2005@gmail.com>
2025-02-20 09:55:50 +0100

    dasas

--- eventosprofesor-1/eventosprofesor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('eventos/', include('eventos_app.urls')),
-    path('usuarios/', include('usuarios_app.urls')),
-]
