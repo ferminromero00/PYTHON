5b90c8be8ef8e140e3cffef0a193d8e6987264b7
Fermin <ferminromero2005@gmail.com>
2025-02-24 13:16:39 +0100

    das

--- mywall/mywall/urls.py
+++ mywall/mywall/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from usuarios_app.views import registro, iniciar_sesion, cerrar_sesion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios_app.urls')),
+    path('registro/', registro, name='registro'),
+    path('login/', iniciar_sesion, name='login'),
+    path('logout/', cerrar_sesion, name='logout'),
 ]
+
 
 
--- mywall/usuarios_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import registro, iniciar_sesion, cerrar_sesion
-from django.contrib.auth.views import LogoutView
-
-urlpatterns = [
-    path('registro/', registro, name='registro'),
-    path('login/', iniciar_sesion, name='login'),
-    path('logout/', cerrar_sesion, name='logout'),
-]
