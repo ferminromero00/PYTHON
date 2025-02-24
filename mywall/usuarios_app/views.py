244ebc21968396374d2f9c0396fcd4560286a173
Fermin <ferminromero2005@gmail.com>
2025-02-24 13:10:20 +0100

    dasdas

--- mywall/usuarios_app/views.py
+++ mywall/usuarios_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegistroForm, LoginForm
 
-# Create your views here.
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirige a la página principal
+    else:
+        form = RegistroForm()
+    return render(request, 'usuarios_app/registro.html', {'form': form})
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'usuarios_app/login.html', {'form': form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('login')
