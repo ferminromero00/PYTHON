9020b7cea1c20210fb5320d627942e7041f0739b
Fermin <ferminromero2005@gmail.com>
2025-02-24 13:21:11 +0100

    dsada

--- mywall/mywall/settings.py
+++ mywall/mywall/settings.py
@@ -89,20 +89,6 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'usuarios_app.Usuario'
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 
 # Internationalization
