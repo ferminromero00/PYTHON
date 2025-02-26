91d91ff8485af6a3f461fedd1c462820fa9c9ba8
Fermin <ferminromero2005@gmail.com>
2025-02-26 12:23:37 +0100

    dasdas

--- arbnb/alojamientos_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Alojamiento, Alquiler
-
-# Register your models here.
-
-admin.site.register(Alojamiento)
-admin.site.register(Alquiler)
--- arbnb/alojamientos_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Alojamiento, Alquiler
-from .forms import AlojamientoForm, AlquilerForm
-
-# Create your views here.
