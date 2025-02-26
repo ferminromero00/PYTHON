510b5a25c2139e3f0002d66996b9a5add0af37db
Fermin <ferminromero2005@gmail.com>
2025-02-26 12:28:00 +0100

    dasdas

--- arbnb2/arbnb/urls.py
+++ arbnb2/arbnb/urls.py
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+# urls.py
 from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
 
-
-
-    
 urlpatterns = [
-      
-    path('admin/', admin.site.urls)
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('alojamientos/', views.alojamientos, name='alojamientos'),
 ]
+
