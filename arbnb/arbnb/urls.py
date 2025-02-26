d672ab5c4c335b2992cb6374ed2b9d58bcdfbcfc
Fermin <ferminromero2005@gmail.com>
2025-02-26 11:59:59 +0100

    das

--- arbnb/arbnb/urls.py
+++ arbnb/arbnb/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
+from usuarios_app.views import iniciar_sesion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', iniciar_sesion, name='login'),
 ]
