3b00c79fd3498704da0dcca18c9b1bcf97482cdd
Fermin <ferminromero2005@gmail.com>
2025-02-26 12:06:54 +0100

    dasda

--- arbnb/usuarios_app/views.py
+++ arbnb/usuarios_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout, authenticate, login 
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import LoginForm
+from alojamientos_app.models import Alojamiento
 
 # Create your views here.
 
@@ -11,12 +12,16 @@ def iniciar_sesion(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')
+            return redirect('alojamientos')
     else:
         form = LoginForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'alojamientos.html', {'form': form})
 
         
 def cerrar_sesion(request):
     logout(request)
     return redirect("login")
+
+def mis_alojamientos(request):
+    alojamientos = Alojamiento.objects.filter(propietario=request.user)
+    return render(request, "alojamientos.html", {"alojamientos": alojamientos})
