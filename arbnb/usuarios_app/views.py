3db1e0eb07f2a1a804f4a37fa23fb00048e96485
Fermin <ferminromero2005@gmail.com>
2025-02-26 11:58:33 +0100

    dasdas

--- arbnb/usuarios_app/views.py
+++ arbnb/usuarios_app/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login 
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+
+def iniciar_sesion(request):
+    if request.user.is_authenticated:
+        return redirect("mis_alojamientos")
+
+    if (request.method == "POST"):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("mis_alojamientos")
+        else:
+            form = AuthenticationForm()
+            return render(request, "login.html", {'form': form})
